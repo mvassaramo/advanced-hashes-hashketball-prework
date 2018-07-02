cc58aa26069491afc0529509c5501ab3ef79c27f
Maduri Vassaramo <maduri.vassaramo@hotmail.co.uk>
2018-07-02 17:41:28 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -7,50 +7,50 @@ def game_hash
       :colors => ["Black","White"],
       :players => {
         "Alan Anderson" => {
-        :number => "0",
+        :number => 0,
         :shoe => 16,
         :points => 22,
-        :rebounds => "12",
-        :assists => "12",
-        :steals => "3",
-        :blocks => "1",
-        :slam_dunks => "1" },
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1 },
           "Reggie Evans" => {
-        :number => "30",
+        :number => 30,
         :shoe => 14,
         :points => 12,
-        :rebounds => "12",
-        :assists => "12",
-        :steals => "12",
-        :blocks => "12",
-        :slam_dunks => "7"},
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7},
           "Brook Lopez" => {
-        :number => "11",
+        :number => 11,
         :shoe => 17,
         :points => 17,
-        :rebounds => "19",
-        :assists => "10",
-        :steals => "3",
-        :blocks => "1",
-        :slam_dunks => "15"},
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15},
           "Mason Plumlee" => {
-        :number => "1",
+        :number => 1,
         :shoe => 19,
         :points => 26,
-        :rebounds => "12",
-        :assists => "6",
-        :steals => "3",
-        :blocks => "8",
-        :slam_dunks => "5"},
+        :rebounds => 12,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5},
         "Jason Terry" => {
-        :number => "31",
+        :number => 31,
         :shoe => 15,
         :points => 19,
-        :rebounds => "2",
-        :assists => "2",
-        :steals => "4",
-        :blocks => "11",
-        :slam_dunks => "1" }
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1 }
       }
     },
     :away => {
@@ -58,50 +58,50 @@ def game_hash
       :colors => ["Turquoise","Purple"],
       :players => {
         "Jeff Adrien" => {
-        :number => "4",
+        :number => 4,
         :shoe => 18,
         :points => 10,
-        :rebounds => "1",
-        :assists => "1",
-        :steals => "2",
-        :blocks => "7",
-        :slam_dunks => "2" },
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2 },
           "Bismak Biyombo" => {
-        :number => "0",
+        :number => 0,
         :shoe => 16,
         :points => 12,
-        :rebounds => "4",
-        :assists => "7",
-        :steals => "7",
-        :blocks => "15",
-        :slam_dunks => "10"},
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 7,
+        :blocks => 15,
+        :slam_dunks => 10},
           "DeSagna Diop" => {
-        :number => "2",
+        :number => 2,
         :shoe => 14,
         :points => 24,
-        :rebounds => "12",
-        :assists => "12",
-        :steals => "4",
-        :blocks => "5",
-        :slam_dunks => "5"},
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5},
           "Ben Gordon" => {
-        :number => "8",
+        :number => 8,
         :shoe => 15,
         :points => 33,
-        :rebounds => "3",
-        :assists => "2",
-        :steals => "1",
-        :blocks => "1",
-        :slam_dunks => "0"},
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0},
         "Brendan Haywood" => {
-        :number => "33",
+        :number => 33,
         :shoe => 15,
         :points => 6,
-        :rebounds => "12",
-        :assists => "12",
-        :steals => "22",
-        :blocks => "5",
-        :slam_dunks => "12" }
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 22,
+        :blocks => 5,
+        :slam_dunks => 12 }
       }
     }
 }
@@ -149,15 +149,16 @@ end
 
 
 
-def player_numbers(team)
+def player_numbers(team_name)
+
 
 player_numbers = []
 
-  game_hash.each do |team, data|
+  game_hash.each do |side, data|
   
       data.each do |overview, players|
         
-        if overview == :players 
+        if overview == :players
           
         players.each do |name, stats| 
           
@@ -171,50 +172,37 @@ player_numbers = []
           
           end 
           end 
-       end 
+      end 
     end 
   end 
+  player_numbers
+ 
 end
 
 
-  
-  
-
-
-# home_players = game_hash[:home][:players].keys
-# away_players = game_hash[:away][:players].keys
-
-# player_numbers = []
+ def player_stats(player)
+   if game_hash[:home][:players].keys.include?(player)
+      return game_hash[:home][:players][player]
+  else 
+      game_hash[:away][:players][player]
+  end 
+ end 
  
-# # game_hash.each do |team, data|
+ 
+ def big_shoe_rebounds
    
-#   game_hash[team].each do |players, player_name|
-     
-#     player_name.each do |player, info|
-       
-#       if info == :number
-#         value.each do |number|
-           
-#           player_numbers << number
-#       end 
-#     end 
-#   end 
-# end 
-# end 
-#   return player_numbers
-# end 
+   all_player_shoe = []
+   
+   game_hash[:home][:players].each do |player, data|
+  
+     if data.key == :shoe
+       player_shoe << :shoe.value
+     else   
+     end 
+   end
+   
+ end 
+  
 
- 
-# if team == "Brooklyn Nets"
-#       game_hash[:home][:players].keys.each do 
-#       |number, value| player_numbers << value
-#       end 
-#     binding.pry
-# else 
-#   game_hash[:away][:players].each do
-#       |number, value| player_numbers << value
-#       end 
-# end 
-# return player_numbers
     
  
